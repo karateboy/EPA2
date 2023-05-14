fb9a281b786dafcfdef805b6375cd72e34086fbf
karateboy <karateboy.tw@gmail.com>
2023-05-14 21:41:02 +0800

    1. Integrate Open Data Central DB.

--- app/models/OpenDataReceiver.scala
+++ app/models/OpenDataReceiver.scala
@@ -6,9 +6,11 @@ import models.ModelHelper._
 import play.api.Play.current
 import play.api._
 import play.api.libs.concurrent.Akka
+import play.api.libs.json.{JsError, Json, Reads}
 import play.api.libs.ws._
 import scalikejdbc._
 
+import scala.collection.mutable
 import scala.concurrent.ExecutionContext.Implicits.global
 
 object OpenDataReceiver {
@@ -26,6 +28,25 @@ object OpenDataReceiver {
 
   case object GetEpaHourData
   case class ReloadEpaData(start:DateTime, end:DateTime)
+
+  val mtNameToMonitorType: Map[String, _root_.models.MonitorType.Value] = Map(
+    "PM10" -> MonitorType.withName("PM10"),
+    "PM25" -> MonitorType.withName("PM25"),
+    "SO2" -> MonitorType.withName("SO2"),
+    "NOX" -> MonitorType.withName("NOx"),
+    "CO" -> MonitorType.withName("CO"),
+    "O3" -> MonitorType.withName("O3"),
+    "THC" -> MonitorType.withName("THC"),
+    "NO" -> MonitorType.withName("NO"),
+    "CH4" -> MonitorType.withName("CH4"),
+    "NO2" -> MonitorType.withName("NO2"),
+    "NMHC" -> MonitorType.withName("NMHC"),
+    "WD_SPEED" -> MonitorType.withName("WD_SPEED"),
+    "WD_DIR" -> MonitorType.withName("WD_DIR"),
+    "RAIN" -> MonitorType.withName("RAIN"),
+    "TEMP" -> MonitorType.withName("TEMP"),
+    "HUMID" -> MonitorType.withName("HUMID")
+  )
 }
 
 import java.util.Date
@@ -40,6 +61,7 @@ case class HourData(
                      MonitorDate: Date,
                      MonitorValues: Seq[Double])
 
+case class OpenDataConfig(enable: Boolean, upstream: String)
 class OpenDataReceiver extends Actor with ActorLogging {
 
   import OpenDataReceiver._
@@ -52,18 +74,121 @@ class OpenDataReceiver extends Actor with ActorLogging {
 
   import scala.xml._
 
-  def receive = {
+  private val openDataConfig = {
+    val config = current.configuration
+    val enable = config.getBoolean("openData.enable").getOrElse(false)
+    val upstream = config.getString("openData.upstream").getOrElse("")
+    OpenDataConfig(enable, upstream)
+  }
+  Logger.info(s"OpenDataReceiver start: openDataConfig: $openDataConfig")
+
+  def receive: Receive = {
     case GetEpaHourData =>
-      val start = SystemConfig.getEpaLast
-      val end = DateTime.now().withMillisOfDay(0)
-      if (start < end) {
-        Logger.info(s"Get EpaData ${start.toString("yyyy-MM-d")} => ${end.toString("yyyy-MM-d")}")
-        getEpaHourData(start, end)
+      if (openDataConfig.enable) {
+        val start = SystemConfig.getEpaLast
+        val end = DateTime.now().withMillisOfDay(0)
+        if (start < end) {
+          Logger.info(s"Get EpaData ${start.toString("yyyy-MM-d")} => ${end.toString("yyyy-MM-d")}")
+          fetchOpenEpaData(start, end)
+        }
       }
 
     case ReloadEpaData(start, end) =>
-      Logger.info(s"reload EpaData ${start.toString("yyyy-MM-d")} => ${end.toString("yyyy-MM-d")}")
-      getEpaHourData(start, end)
+      if (openDataConfig.enable) {
+        Logger.info(s"reload EpaData ${start.toString("yyyy-MM-d")} => ${end.toString("yyyy-MM-d")}")
+        fetchOpenEpaData(start, end)
+      }
+  }
+
+  case class MtRecord(mtName: String, value: Option[Double], var status: String)
+
+  case class RecordListID(time: Date, monitor: String)
+
+  case class RecordList(var mtDataList: Seq[MtRecord], _id: RecordListID) {
+    def mtMap: Map[String, MtRecord] = {
+      val pairs =
+        mtDataList map { data => data.mtName -> data }
+      pairs.toMap
+    }
+  }
+
+  implicit val r1: Reads[RecordListID] = Json.reads[RecordListID]
+  implicit val r2: Reads[MtRecord] = Json.reads[MtRecord]
+  implicit val r3: Reads[RecordList] = Json.reads[RecordList]
+
+  private def fetchOpenEpaData(start: DateTime, end: DateTime): Unit = {
+    val epaMonitorsIDs = EpaMonitor.idMap.keys.map(id => s"Epa$id").mkString(":")
+    val startNum = start.getMillis
+    val endNum = end.getMillis
+    val f = WS.url(s"${openDataConfig.upstream}/HourRecord/$epaMonitorsIDs/$startNum/$endNum").get()
+    f onFailure errorHandler()
+    for (response <- f) yield {
+      val ret = response.json.validate[Seq[RecordList]]
+      ret.fold(
+        err => {
+          Logger.error(JsError.toJson(err).toString())
+          false
+        },
+        recordLists => {
+          Logger.info(s"Total ${recordLists.size} records fetched.")
+          upsertRecordLists(recordLists)
+          true
+        }
+      )
+    }
+
+    def upsertRecordLists(recordLists: Seq[RecordList]): Int = {
+      import scala.collection.mutable.Map
+      val recordMap = mutable.Map.empty[EpaMonitor.Value, mutable.Map[DateTime, mutable.Map[MonitorType.Value, Double]]]
+
+      recordLists.foreach(recordList => {
+        val epaMonitor = EpaMonitor.idMap(recordList._id.monitor.drop(3).toInt)
+        val time = new DateTime(recordList._id.time)
+        val timeMap = recordMap.getOrElseUpdate(epaMonitor, mutable.Map.empty[DateTime, mutable.Map[MonitorType.Value, Double]])
+        recordList.mtDataList.foreach(mtData => {
+          val mtMap = timeMap.getOrElseUpdate(time, mutable.Map.empty[MonitorType.Value, Double])
+          for (mt <- mtNameToMonitorType.get(mtData.mtName); value <- mtData.value)
+            mtMap.put(mt, value)
+        })
+      })
+
+      val updateCounts =
+        for {
+          monitorMap <- recordMap
+          monitor = monitorMap._1
+          timeMaps = monitorMap._2
+          dateTime <- timeMaps.keys.toList.sorted
+          mtValue <- timeMaps(dateTime)
+        } yield {
+          val MStation = EpaMonitor.map(monitor)
+          val MItem = MonitorType.map(mtValue._1).epa_mapping.get
+          val MDate: java.sql.Timestamp = dateTime
+          try {
+            DB autoCommit { implicit session =>
+              sql"""
+                UPDATE dbo.hour_data
+                SET MValue = ${mtValue._2}
+                WHERE MStation=${MStation.id} and MDate=${MDate} and MItem=${MItem};
+
+                IF(@@ROWCOUNT = 0)
+                BEGIN
+                  INSERT INTO dbo.hour_data (MStation, MDate, MItem, MValue)
+                  VALUES(${MStation.id}, ${MDate}, ${MItem}, ${mtValue._2})
+                END
+              """.update.apply
+            }
+          } catch {
+            case ex: Exception =>
+              Logger.error(s"upsertRecordLists error: ${ex.getMessage}", ex)
+              0
+          }
+        }
+
+      if (updateCounts.sum != 0)
+        Logger.info(s"EPA ${updateCounts.sum} records have been upserted.")
+
+      recordLists.size
+    }
   }
 
   def getEpaHourData(start: DateTime, end: DateTime) {
@@ -210,7 +335,7 @@ class OpenDataReceiver extends Actor with ActorLogging {
             getMonthData(year, month, skip + limit)
         } catch {
           case ex: Exception =>
-            Logger.error(ex.toString())
+            Logger.error(ex.toString)
             throw ex
         }
       }
@@ -219,7 +344,7 @@ class OpenDataReceiver extends Actor with ActorLogging {
     if(start.toString("yyyy-M") == DateTime.now().toString("yyyy-M"))
       getThisMonth(0)
     else{
-      getMonthData(start.getYear(), start.getMonthOfYear(), 0)
+      getMonthData(start.getYear, start.getMonthOfYear, 0)
     }
   }
 
